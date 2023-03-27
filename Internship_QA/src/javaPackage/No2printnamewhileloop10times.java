65a7fc0d6d7d6dde2d026316f9735b24791fcd7b
Rutgers <professionalrutgers@gmail.com>
2023-03-27 15:11:53 -0400

    add 3 more java programs

---
+++ Internship_QA/src/javaPackage/No2printnamewhileloop10times.java
@@ -0,0 +1,13 @@
+package javaPackage;
+
+public class No2printnamewhileloop10times {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=0;
+		while(a<=10)
+		{System.out.println("Vasundara");
+		a++;}
+	}
+
+}
