d24a1e7dd3ccd878793b84f235ef5ba702f7dc68
Rutgers <professionalrutgers@gmail.com>
2023-03-27 16:04:25 -0400

    adding program5

---
+++ Internship_QA/src/javaPackage/No5customerclass.java
@@ -0,0 +1,56 @@
+package javaPackage;
+
+public class No5customerclass {
+	String name;
+	String sex;
+	String address;
+	int salary;
+		
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				No5customerclass obj1=new No5customerclass();
+		obj1.name="kannan";
+		obj1.sex="Male";
+		obj1.address="Gopi street,Ayarpaadi";
+		obj1.salary= 20000;
+
+		System.out.println("Name of the customer is "+obj1.name);
+		System.out.println("Address of the customer is "+obj1.address);
+		System.out.println("The customer is a "+obj1.sex);
+		System.out.println("Salary of the customer is "+obj1.salary);
+
+		No5customerclass obj2=new No5customerclass();
+		obj2.name="Andal";
+		obj2.sex="Female";
+		obj2.address="Govindaa street,Srivilliputhur";
+		obj2.salary= 40000;
+
+		System.out.println("Name of the customer is "+obj2.name);
+		System.out.println("Address of the customer is "+obj2.address);
+		System.out.println("The customer is a "+obj2.sex);
+		System.out.println("Salary of the customer is "+obj2.salary);
+
+		No5customerclass obj3=new No5customerclass();
+		obj3.name="Lakshmi";
+		obj3.sex="Female";
+		obj3.address="All House,Boologam";
+		obj3.salary= 40000;
+
+		System.out.println("Name of the customer is "+obj3.name);
+		System.out.println("Address of the customer is "+obj3.address);
+		System.out.println("The customer is a "+obj3.sex);
+		System.out.println("Salary of the customer is "+obj3.salary);
+
+		No5customerclass obj4=new No5customerclass();
+		obj4.name="Nappinnai";
+		obj4.sex="Female";
+		obj4.address="Gopi street,Ayarpaadi";
+		obj4.salary= 40000;
+
+		System.out.println("Name of the customer is "+obj4.name);
+		System.out.println("Address of the customer is "+obj4.address);
+		System.out.println("The customer is a "+obj4.sex);
+		System.out.println("Salary of the customer is "+obj4.salary);
+	}
+
+}
